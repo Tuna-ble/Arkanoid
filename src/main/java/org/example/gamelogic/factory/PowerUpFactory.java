cd6575a39d34694e99517d129734de74726443c5
NĐTThanh <truongthanh20042006@gmail.com>
2025-10-17 13:38:14 +0700

    small edit

--- src/main/java/org/example/gamelogic/factory/PowerUpFactory.java
+++ src/main/java/org/example/gamelogic/factory/PowerUpFactory.java
@@ -1,5 +1,6 @@
 package org.example.gamelogic.factory;
 
+import org.example.gamelogic.entities.powerups.PowerUp;
 import org.example.gamelogic.registry.PowerUpRegistry;
 
 public class PowerUpFactory {
@@ -9,12 +10,12 @@ public class PowerUpFactory {
         this.registry = registry;
     }
 
-    public org.example.gamelogic.entities.powerups.PowerUp createPowerUp(String powerUpType, double x, double y) {
-        org.example.gamelogic.entities.powerups.PowerUp prototype = registry.getPrototype(powerUpType);
+    public PowerUp createPowerUp(String powerUpType, double x, double y) {
+        PowerUp prototype = registry.getPrototype(powerUpType);
         if (prototype == null) {
             throw new IllegalArgumentException("Prototype not found" + powerUpType);
         }
-        org.example.gamelogic.entities.powerups.PowerUp newPowerUp = prototype.clone();
+        PowerUp newPowerUp = prototype.clone();
         newPowerUp.setPosition(x, y);
         return newPowerUp;
     }
