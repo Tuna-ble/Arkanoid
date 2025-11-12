f0b9394836b1aa1b3250fe25f89e2fba28394c29
Thang Nguyen <dinhthangnguyen2006@gmail.com>
2025-11-12 08:54:48 +0700

    used assets on enemy1

--- src/main/java/org/example/gamelogic/entities/enemy/Enemy1.java
+++ src/main/java/org/example/gamelogic/entities/enemy/Enemy1.java
@@ -1,16 +1,32 @@
 package org.example.gamelogic.entities.enemy;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import org.example.config.GameConstants;
+import org.example.data.AssetManager;
 import org.example.gamelogic.strategy.movement.DownMovementStrategy;
+import org.example.presentation.SpriteAnimation;
 
 public class Enemy1 extends AbstractEnemy {
+    private Image enemyImage;
+    private SpriteAnimation idleAnimation;
     public Enemy1(double x, double y, double width, double height,
                             double dx, double dy) {
         super(x, y, width, height, dx, dy, new DownMovementStrategy());
         this.health = 1;
         this.scoreValue = 100;
+        AssetManager am = AssetManager.getInstance();
+        this.enemyImage = am.getImage("enemy1");
+        if (enemyImage != null) {
+            int frameCount = 5;
+            int columns = 8;
+            double duration = 2;
+            boolean loops = true;
+            this.idleAnimation = new SpriteAnimation(
+                    enemyImage, frameCount, columns, duration, loops
+            );
+        }
     }
 
     @Override
@@ -21,6 +37,9 @@ public class Enemy1 extends AbstractEnemy {
     @Override
     public void update(double deltaTime) {
         super.update(deltaTime);
+        if (idleAnimation != null) {
+            idleAnimation.update(deltaTime);
+        }
     }
 
     @Override
@@ -34,10 +53,7 @@ public class Enemy1 extends AbstractEnemy {
 
     @Override
     public void render(GraphicsContext gc) {
-        gc.setFill(Color.BROWN);
-        gc.fillRect(x, y, width, height);
-        gc.setStroke(Color.BLACK);
-        gc.strokeRect(x, y, width, height);
+        idleAnimation.render(gc, x, y, width, height);
     }
 
     public void takeDamage(double damage) {
