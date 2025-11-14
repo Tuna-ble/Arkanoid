0de6e1f3eded97c37a92a3aefe173fc392233110
Thang Nguyen <dinhthangnguyen2006@gmail.com>
2025-11-14 07:58:05 +0700

    repackaged buttons

--- src/main/java/org/example/gamelogic/graphics/buttons/AbstractButton.java
+++ src/main/java/org/example/gamelogic/graphics/buttons/AbstractButton.java
@@ -1,15 +1,10 @@
-package org.example.gamelogic.graphics.Buttons;
+package org.example.gamelogic.graphics.buttons;
 
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.TextAlignment;
-import org.example.config.GameConstants;
 import org.example.data.AssetManager;
 import org.example.gamelogic.I_InputProvider;
-import org.example.gamelogic.graphics.TextRenderer;
 
 public abstract class AbstractButton {
     protected double x;
