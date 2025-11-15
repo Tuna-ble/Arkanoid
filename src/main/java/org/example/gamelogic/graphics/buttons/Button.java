167e5bd52e1752cff468da3c20f1d4b193b43f05
Thang Nguyen <dinhthangnguyen2006@gmail.com>
2025-11-16 03:28:49 +0700

    small fix

--- src/main/java/org/example/gamelogic/graphics/buttons/Button.java
+++ src/main/java/org/example/gamelogic/graphics/buttons/Button.java
@@ -4,6 +4,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import org.example.config.GameConstants;
 import org.example.gamelogic.graphics.TextRenderer;
@@ -11,12 +12,19 @@ import org.example.gamelogic.graphics.TextRenderer;
 public class Button extends AbstractButton {
     private Image normalImage;
     private Image hoverImage;
+    private static final double FONT_SCALE_RATIO = 0.5;
+    private static final String DEFAULT_FONT_FAMILY = "Anxel";
 
     public Button(double x, double y, double width, double height,
                   Image normalImage, Image hoverImage, String text) {
         super(x, y, width, height, text);
         this.normalImage = normalImage;
         this.hoverImage = (hoverImage != null) ? hoverImage : normalImage;
+
+        double dynamicFontSize = height * FONT_SCALE_RATIO;
+
+        this.font = new Font(DEFAULT_FONT_FAMILY, dynamicFontSize);
+        this.textColor = Color.WHITE;
     }
 
     public Button(double x, double y, Image normalImage, Image hoverImage, String text) {
@@ -37,8 +45,8 @@ public class Button extends AbstractButton {
             TextRenderer.drawOutlinedText(
                     gc,
                     text,
-                    x + width / 2,
-                    y + height / 2 + 8,
+                    x + width / 2 + 2,
+                    y + height / 2 + 9,
                     font,
                     textColor,
                     Color.color(0, 0, 0, 0.85),
