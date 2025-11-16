438aa8cfec31de896c869394c80895dfa3094248
Thang Nguyen <dinhthangnguyen2006@gmail.com>
2025-11-17 01:02:13 +0700

    assets for bullet

--- src/main/java/org/example/gamelogic/entities/BulletType.java
+++ src/main/java/org/example/gamelogic/entities/BulletType.java
@@ -1,9 +1,9 @@
 package org.example.gamelogic.entities;
 
 public enum BulletType {
-    PLAYER_LASER(4, 20),
-    BOSS_HOMING_SQUARE(10, 10),
-    BOSS_LASER(8, 40),
+    PLAYER_LASER(7, 20),
+    BOSS_HOMING_SQUARE(15, 15),
+    BOSS_LASER(20, 60),
     ENEMY_LASER(4, 20);
 
     public final double width;
--- src/main/java/org/example/gamelogic/entities/LaserBullet.java
+++ src/main/java/org/example/gamelogic/entities/LaserBullet.java
@@ -1,20 +1,36 @@
 package org.example.gamelogic.entities;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import org.example.config.GameConstants;
+import org.example.data.AssetManager;
+import org.example.presentation.SpriteAnimation;
 
 public class LaserBullet extends MovableObject {
     private final BulletFrom faction;
+    private final BulletType type;
+    private final Image normalBulletImage;
+    private final Image bossBulletSheet;
+    private final SpriteAnimation bossBullet;
+
     public LaserBullet(double x, double y, double dx, double dy, BulletType type, BulletFrom faction) {
         super(x, y, type.width, type.height, dx, dy);
         this.faction = faction;
+        this.type = type;
+
+        AssetManager am = AssetManager.getInstance();
+        normalBulletImage = am.getImage("bullet");
+        bossBulletSheet = am.getImage("bossBullet");
+        bossBullet = new SpriteAnimation(bossBulletSheet, 4, 4, 0.5, true);
     }
 
     @Override
     public void update(double deltaTime) {
         x += dx * deltaTime;
         y += dy * deltaTime;
+
+        bossBullet.update(deltaTime);
 
         if (y + height < 0 || y > GameConstants.SCREEN_HEIGHT ||
                 x + width < 0 || x > GameConstants.SCREEN_WIDTH) {
@@ -25,13 +41,16 @@ public class LaserBullet extends MovableObject {
     @Override
     public void render(GraphicsContext gc) {
         if (faction == BulletFrom.PLAYER) {
-            gc.setFill(Color.CYAN); // Đạn ta
+            gc.drawImage(normalBulletImage, x, y, width, height);
         } else {
-            gc.setFill(Color.RED); // Đạn địch
+            if (type == BulletType.BOSS_HOMING_SQUARE) {
+                bossBullet.render(gc, x, y, width, height);
+            } else {
+                gc.setFill(Color.RED);
+                gc.fillRect(x, y, width, height);
+            }
         }
-        gc.fillRect(x, y, width, height);
-        gc.setStroke(Color.BLACK);
-        gc.strokeRect(x, y, width, height);
+
     }
 
     public BulletFrom getFaction() {
